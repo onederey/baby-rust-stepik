4327097a52030d32d2fb18d2121b029cfa28ea80
onederey <morozovcustom@yandex.ru>
2025-01-08 00:30:38 +0400

    добавил пользовательский ввод
    а также конвертацию строки в число

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    println!("Пожалуйста, введите свою строку:");
+
+    let mut user_input = String::new();
+
+    let result = io::stdin()
+        .read_line(&mut user_input)
+        .expect("Не удалось прочитать ввод!");
+
+    println!("result = {}", result);
+
+    println!("Вы ввели (trim): {}", user_input.trim());
+    println!("Вы ввели (raw): {:#?}", user_input);
+
+    let parsed_input_to_int = user_input
+        .trim()
+        .parse::<i32>()
+        .expect("Пожалуйста, введите число!");
+
+    println!("Вы ввели число: {}", parsed_input_to_int);
 }
